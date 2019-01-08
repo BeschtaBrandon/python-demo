9527c508f0c78edee294edc12084dd9d982b413c
BeschtaBrandon <blbeschta@wisc.edu>
2019-01-07 20:36:16 -0600

    Initial commit

---
+++ Node.py
@@ -0,0 +1,41 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def findval(self, lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                return str(lkpval)+" Not Found"
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                return str(lkpval)+" Not Found"
+            return self.right.findval(lkpval)
+        else:
+            print(str(self.data) + ' is found')
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print( self.data),
+        if self.right:
+            self.right.printTree()
---
+++ main.py
@@ -0,0 +1,9 @@
+from Node import  Node
+
+root = Node(16)
+
+root.insert(6)
+root.insert(12)
+root.insert(3)
+
+root.printTree()
